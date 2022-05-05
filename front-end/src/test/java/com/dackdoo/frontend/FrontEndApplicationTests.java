c539e392acb13344d7e6e67a78c44a3d855183d5
dooyoungk <dackdoo@naver.com>
2022-05-05 23:40:35 +0900

    Initial commit

---
+++ front-end/src/test/java/com/dackdoo/frontend/FrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dackdoo.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
