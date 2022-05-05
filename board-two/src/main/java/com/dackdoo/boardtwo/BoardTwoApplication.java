0895ec15a9502e14d601dd2f19bb9dde2db7e730
dooyoungk <dackdoo@naver.com>
2022-05-06 08:06:04 +0900

    eureka update commit

--- board-two/src/main/java/com/dackdoo/boardtwo/BoardTwoApplication.java
+++ board-two/src/main/java/com/dackdoo/boardtwo/BoardTwoApplication.java
@@ -2,8 +2,10 @@ package com.dackdoo.boardtwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BoardTwoApplication {
 
 	public static void main(String[] args) {
